acb30b2a56af823d34a44666a97a38f654a4f6c3
Nick Robison <nicholas.a.robison@omb.eop.gov>
2020-01-15 13:15:12 -0500

    Added cmdliner support to cli.

--- bin/main.ml
+++ bin/main.ml
@@ -1,24 +1,40 @@
 open! Base
 open Lib
+open Cmdliner
 
 exception ParseError of string
 
-let file = "patient-stu3.json"
-
 let parse_definition ic =
   let json = Yojson.Safe.from_channel ic in
-  let s = Structure.of_yojson json in
-  match s with
-  | Ok s -> s
-  | Error e -> raise (ParseError e)
+  Structure.t_of_yojson json
 
-let () =
-  let ic = In_channel.create file in
+let parse file =
+  let ic = Stdio.In_channel.create file in
   ( try
       let def = parse_definition ic in
-      print_endline( "Parsed: " ^ Structure.typ def);
-      In_channel.close ic
-    with e ->
-      In_channel.close ic;
-      raise e);
-  print_endline "Hello, World!"
+      Stdio.print_endline( "Parsed: " ^ Structure.typ def)
+    with End_of_file ->
+      Stdio.In_channel.close ic);
+  Stdio.print_endline "Hello, World!"
+
+let file =
+  let _doc = "Input file to parse." in
+  Arg.(required & pos 0 (some string) None & info [] ~docv:"FILE")
+
+let version =
+  match (Build_info.V1.version ()) with
+  | None -> "n/a"
+  | Some v -> Build_info.V1.Version.to_string v
+
+let parse_t = Term.(const parse $ file)
+
+let info =
+  let doc = "FHIR language compiler" in
+  let man = [
+    `S Manpage.s_bugs;
+    `P "Email bug reports to <nicholas.a.robison@omb.eop.gov>."
+  ] in
+  Term.info "fhirc" ~version ~doc ~exits: Term.default_exits ~man
+
+let () =
+  Term.exit @@ Term.eval (parse_t, info)
